f7b1992e277594fccb786dd0400aa85b714e3372
Umeshchaurasiya123 <umeshc1998@gmail.com>
2025-04-30 01:20:22 +0530

    Add error handling for channel and subscriber ID validation in subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -98,11 +98,53 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    if(!channelId?.trim()){
+        throw new ApiError(400, "Channel id is required")
+    }
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    const subscribers = await Subscription.find({
+        channel:channelId
+    })
+
+    if(!subscribers){
+        throw new ApiError(404, "No subscribers found")
+    }
+   
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, subscribers, "Subscribers of fetched successfully"))
+
+    
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+
+    if (!subscriberId?.trim()) {
+        throw new ApiError(400, "Subscriber id is required")
+    }
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriber id")
+    }
+
+    const subscriptions = await Subscription.find({
+        subscriber: subscriberId,
+    })
+
+    if (!subscriptions) {
+        throw new ApiError(404, "User has not subscribed to any channels")
+    }
+
+    res.status(200).json(
+        new ApiResponse(200, subscriptions, "Subscribed channels fetched successfully")
+    )
+    
+
 })
 
 export {
