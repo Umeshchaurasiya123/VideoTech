6c9a7d56667ac8e292968dd6b0db3b2275c7d5a7
Umeshchaurasiya123 <umeshc1998@gmail.com>
2025-05-02 21:57:52 +0530

    Refactor server startup to log the assigned port dynamically

--- src/index.js
+++ src/index.js
@@ -10,8 +10,10 @@ dotenv.config({
 
 connectDB()
 .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    const server= app.listen(process.env.PORT || 0, () => {
+        const assignedPort = server.address().port
+        // console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+        console.log(`⚙️ Server is running at port : ${assignedPort}`);
     })
 })
 .catch((err) => {
